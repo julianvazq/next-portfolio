19782c9d39628ac5c91b925683560375e03ab376
Julian Vazquez <julianvazquez101@gmail.com>
2020-05-19 17:12:11 -0400

    Create next project

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { Title } from '../styles/shared-styles';
+
+export default function Home() {
+  return (
+    <>
+      <Title>My page</Title>
+      <p>asdasd</p>
+    </>
+  );
+}
